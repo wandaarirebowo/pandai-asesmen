760f70065782985bd8324b236f996be4e5de840b
wandaarirebowo <wandaarirebowo@gmail.com>
2026-07-11 21:49:18 +0700

    Versi 11.7.2026.8

--- config.js
+++ config.js
@@ -7,5 +7,5 @@
 window.PANDAI_CONFIG = {
   APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbzUBANzlfmICZrZjKqa2N-pARSwOzOf5CdxuDzZXyKxqLTw72ytDM1aAAqti5g4zKez/exec",
   NAMA_DINAS: "[Dinas Pendidikan Kabupaten/Kota]",
-  VERSI: "1.0.0-m1"
+  VERSI: "1.0.0-m3"
 };
